0bdb2982940e6ed9eb28e60df53e584d9af7569c
gabrielbarcik <gbarcike@gmail.com>
2019-11-27 22:33:55 +0100

    include priority sorting to explore nodes

--- dfs.py
+++ dfs.py
@@ -3,7 +3,9 @@
 import os
 import numpy as np
 import networkx as nx
+from matplotlib import pyplot as plt
 import time
+from graph_generation import GraphGenerator
 from IPython.display import clear_output
 
 def prepare_x_DFS(graph, root=0):
@@ -11,9 +13,9 @@ def prepare_x_DFS(graph, root=0):
     Parameters
     ----------
     graph: NetworkX Graph instance
-        The graph on which the algorithm should be run
+    The graph on which the algorithm should be run
     source: index of the node that should be used as a root for the DFS
-    
+
     Returns:
     --------
     Initialized numpy representation of the graph, as used by our DFS implementation
@@ -31,34 +33,37 @@ def iter_DFS(x, E):
     Parameters
     ----------
     x: numpy array
-        array of the node's features.
-        At initialization, x[i] should be 1 for the source node and 0 otherwise
+    array of the node's features.
+    At initialization, x[i] should be 1 for the source node and 0 otherwise
     E: numpy array
-        adjacency matrix. E[i,j]=1 indicates a edge from node i to node j
-    
+    adjacency matrix. E[i,j]=1 indicates a edge from node i to node j
+
     Returns
     -------
     Modifies x, using our DFS algorithm
     '''
 
     i0 = np.argmax(x) # Select the node with highest rank
-    next_label = x[i0]+1 # Detect rank to assign
+    next_label = x[i0] + 1 # Detect rank to assign
 
-    x[i0] = np.amin(x)-1 # Mark node as seen. Implicitely encodes the position in which the node was seen
-    
-    neigh = np.argwhere(E[i0]==1)[:,1] # Select the neighbourgs of this node
-        
+    x[i0] = np.amin(x) - 1 # Mark node as seen. Implicitely encodes the position in which the node was seen
+
+    neigh = np.argwhere(E[i0]==1)[:,1] # Select the neighbours of this node
+
+    neigh = sorted(neigh, key=lambda id: graph.nodes[id]['priority'], reverse=True)
+
     for ind in neigh:
         # If son was not explored, update it it
         if x[ind] == 0:
             x[ind] = next_label # Mark the sons with highest rank, so that it is explored in priority
             next_label += 1 # Update highest rank
-    
 
 
 if __name__=="__main__":
-    graph = graph_ex = nx.balanced_tree(2,3)
-    root = 2
+    # graph = nx.balanced_tree(2,3)
+    root= 2
+    generator = GraphGenerator()
+    graph = generator.gen_graph_type(10, 'erdos_renyi')
 
     E = nx.to_numpy_matrix(graph)
     x = prepare_x_DFS(graph, root)
@@ -67,5 +72,12 @@ if __name__=="__main__":
     while np.amax(x)>=0:
         iter_DFS(x,E)
         print(x)
-        time.sleep(5)
+#        time.sleep(5)
         clear_output()
+
+    print('DFS output: {}'.format(np.argsort(-x)))
+
+    labels = dict((n, [n, np.around(d['priority'], decimals=2)]) for n, d in graph.nodes(data=True))
+    nx.draw(graph, labels=labels)
+    plt.show()
+
