88f371249d43c7a21df8841e4dad5ee992a00e4b
gabrielbarcik <gbarcike@gmail.com>
2019-11-27 20:23:17 +0100

    add priority feature to the graph

--- graph_generation.py
+++ graph_generation.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import numpy as np
 from matplotlib import pyplot as plt
 
 class GraphGenerator:
@@ -6,27 +7,35 @@ class GraphGenerator:
         pass
 
     def gen_graph_type(self, nb_nodes, graph_type):
+        g = None
+
         if graph_type == 'ladder':
-            return nx.ladder_graph(nb_nodes)
+            g = nx.ladder_graph(nb_nodes)
 
         if graph_type == 'grid':
-            return nx.grid_2d_graph(nb_nodes, nb_nodes // 2 + 1)
+            g = nx.grid_2d_graph(nb_nodes, nb_nodes // 2 + 1)
 
         if graph_type == 'erdos_renyi':
             p = min(np.log(nb_nodes)/nb_nodes, 0.5)
-            return nx.erdos_renyi_graph(n=nb_nodes, p=p, directed=True)
+            g = nx.erdos_renyi_graph(n=nb_nodes, p=p, directed=True)
 
         if graph_type == 'barabasi_albert':
             nb_neighs = 5
-            return nx.barabasi_albert_graph(n=nb_nodes, m=nb_neighs)
+            g = nx.barabasi_albert_graph(n=nb_nodes, m=nb_neighs)
 
         if graph_type == '4_caveman':
             # l (int) – Number of groups
             # k (int) – Size of cliques
             # p (float) – Probabilty of rewiring each edge.
-            return  nx.relaxed_caveman_graph(l=4, k=5, p=0.3)
+            g = nx.relaxed_caveman_graph(l=4, k=5, p=0.3)
 
-        raise ValueError
+        if g is None:
+            raise ValueError
+        else:
+            priorities = {i: p for i, p in
+                          enumerate(np.random.random(len(g.nodes)))}
+            nx.set_node_attributes(g, priorities, name='priority')
+            return g
 
 
 if __name__ == '__main__':
@@ -35,4 +44,5 @@ if __name__ == '__main__':
     nx.draw(g.to_directed())
     plt.show()
     print(nx.adjacency_matrix(g).todense())
+    print(g.nodes(data=True))
 
