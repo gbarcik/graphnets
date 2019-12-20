8cf8a45247917374fe38056f9becd7465a672a10
gabrielbarcik <gbarcike@gmail.com>
2019-12-20 18:50:34 +0100

    clean code

--- training.py
+++ training.py
@@ -44,17 +44,8 @@ data_gen = DatasetGenerator()
 graphs, history_dataset = data_gen.run(graph_type, nb_graphs, nb_nodes,
                                 algorithm_type)
 
-#print(history_dataset[:5])
-
-#print(history_dataset[5000])
-
 print('Dataset created in:', time.time()-start)
 clock = time.time()
-
-# Inspect some shapes
-for i in range(10):
-    pass
-    #print(history_dataset[i].shape)
 
 # Prepare the data in an easily exploitable format
 # It could probably be optimised with DGL
@@ -67,11 +58,13 @@ for i in range(nb_graphs):
     states = history_dataset[i]
     termination = np.zeros(states.shape[0])
     termination[-1] = 1
-    # Adding self edge to every node, as in the paper
+    
+    # TODO?: Adding self edge to every node, as in the paper
     '''for j in range(nb_nodes):
         graphs[i].add_edge(j, j)'''
 
     assert max_steps >= states.shape[0]
+
     # set states to fixed lenght with termination boolean to be able to compute termination error
     if states.shape[0] < max_steps:
         pad_idx = [(0,0) for i in range(states.ndim)]
@@ -142,24 +135,12 @@ for epoch in range(nb_epochs):
 
         states = torch.from_numpy(states)
         edges_mat = torch.from_numpy(edges_mat)
-
-        # What exatly do we want as input?
-
         termination = torch.from_numpy(termination)
 
         if use_cuda:
             states, edges_mat, termination = states.cuda(), edges_mat.cuda(), termination.cuda()
         
         preds, pred_stops = model(graph, states, edges_mat)
-
-        '''if batch_idx==1:
-            print('states:', states)
-            print('pred:', preds)
-            print('termination:', termination)
-            print('pred_stops:', pred_stops)'''
-
-        #print(preds.size())
-        #print(states.size())
 
         loss = nll_gaussian(preds, torch.t(states))
         loss += ((pred_stops-termination)**2).sum()/max_steps # MSE of output and states + termination loss
